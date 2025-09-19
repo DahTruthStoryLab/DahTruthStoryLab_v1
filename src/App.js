d95ee1bbe820f725334bf9899716d080d3bb6334
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-18 21:12:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import RegistrationPage from './components/RegistrationPage';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth/register" element={<RegistrationPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
