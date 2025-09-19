b9f09c8c06c9e0065a66a5ba3792efb33c369aa8
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-19 10:49:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import RegistrationPage from './components/RegistrationPage';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth/register" element={<RegistrationPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
