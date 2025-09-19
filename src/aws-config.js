b0c109da5f29efb3c67f7b5f4f84234809ca9558
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-19 17:29:07 -0400

    Update aws-config.js

--- src/aws-config.js
+++ src/aws-config.js
@@ -2,9 +2,10 @@ import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
-    region: 'us-east-2',
-    userPoolId: 'us-east-2_FGE9qe56F',
-    userPoolWebClientId: '5scd368qedv001d8n687l3lbf2',
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
+    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID, // from the NO-secret client
     mandatorySignIn: false,
-  }
+  },
 });
+
