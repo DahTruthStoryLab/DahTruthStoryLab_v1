28ee24467ec2ca6617bef1b4ba54111b579b8b59
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-20 12:42:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './aws-config';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,3 +10,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
