5b12c84c372128d8fd2c8b11a0294f045323e39e
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-21 18:06:36 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// 👇 Add these lines:
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
-// Configure Amplify once, before React renders
 Amplify.configure(awsconfig);
-
-// (optional) quick sanity check in the console:
-// import { Auth } from 'aws-amplify';
-// console.log('Auth flow:', Auth.configure().authenticationFlowType);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
