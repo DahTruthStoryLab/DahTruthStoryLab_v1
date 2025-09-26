607bec76884d84af303636e7510a7cf0a680e61a
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-26 16:43:04 -0400

    Update BrandLogo.jsx

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,6 +1,4 @@
 // src/components/BrandLogo.jsx
-import logo from "/src/assets/brand/logo-light.svg";
-
 export default function BrandLogo({ className = "h-8 w-auto", alt = "DahTruth" }) {
-  return <img src={logo} alt={alt} className={className} draggable={false} />;
+  return <img src="/DahTruthLogo.png" alt={alt} className={className} draggable={false} />;
 }
