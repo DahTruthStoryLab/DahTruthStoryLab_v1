281c2d26414a33efbbcd2c153c4e336a85a262f5
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-25 22:26:23 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 // src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
+// ✅ Global styles (Tailwind + navy theme)
+import "./index.css";
 
 // ✅ Amplify
 import { Amplify } from "aws-amplify";
-import awsconfig from "./aws-exports"; // make sure this file exists
+import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
-
-// Optional: global styles
-// import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
