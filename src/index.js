108db5a1b21b610fea3bffff787a9bd0f4a81345
DahTruthStoryLab <jmbeausby@aol.com>
2025-09-26 19:02:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 // src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // ← TEMP: HashRouter to bypass rewrite quirks
 import App from "./App";
 import "./index.css";
 
-// Safe Amplify init (won’t crash if aws-exports is missing or misconfigured)
+// Safe Amplify init (won’t crash if aws-exports.js is missing)
 try {
   const { Amplify } = require("aws-amplify");
   const awsconfig = require("./aws-exports");
   Amplify.configure(awsconfig.default || awsconfig);
-  console.log("[Amplify] configured");
 } catch (e) {
-  console.log("[Amplify] not configured – running without backend");
+  console.log("Amplify not configured - continuing without backend");
 }
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+const rootEl = document.getElementById("root");
+const root = createRoot(rootEl);
+root.render(<App />);
