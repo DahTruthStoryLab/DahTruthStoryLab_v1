a67ce561fe3fa357c8e7875a189d811099847075
DahTruthStoryLab <jmbeausby@aol.com>
2025-10-12 19:28:06 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3001", // your Express port
+    },
+  },
+});
+
 export default defineConfig({
   plugins: [
     react({
