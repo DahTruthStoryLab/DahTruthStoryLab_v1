06e020532def38fc1a8fd629b4580ecd61853227
DahTruthStoryLab <jmbeausby@aol.com>
2025-10-13 13:59:41 -0400

    Update resizeImage.js

--- src/lib/image/resizeImage.js
+++ src/lib/image/resizeImage.js
@@ -1,16 +1,12 @@
 export async function resizeImageBlob(blob, maxW = 2000, maxH = 2000, quality = 0.9) {
   const img = await blobToImage(blob);
-
   const scale = Math.min(maxW / img.naturalWidth, maxH / img.naturalHeight, 1);
   const w = Math.round(img.naturalWidth * scale);
   const h = Math.round(img.naturalHeight * scale);
-
   const canvas = document.createElement("canvas");
-  canvas.width = w;
-  canvas.height = h;
+  canvas.width = w; canvas.height = h;
   const ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0, w, h);
-
   return await new Promise((res) => canvas.toBlob((b) => res(b), "image/jpeg", quality));
 }
 
@@ -23,3 +19,4 @@ function blobToImage(blob) {
     img.src = url;
   });
 }
+
