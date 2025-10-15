71e903c69a7dca4b1747f947a597c7fb8962b4dc
DahTruthStoryLab <jmbeausby@aol.com>
2025-10-14 21:14:51 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,30 @@
+// src/main.tsx (or src/index.tsx)
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
-const rootEl = document.getElementById("root");
-if (!rootEl) {
-  throw new Error('Root element #root not found. Make sure public/index.html has <div id="root"></div>.');
+// If you use global providers, uncomment and import them:
+// import { UserProvider } from "./state/UserProvider";
+// import { DndProvider } from "react-dnd";
+// import { HTML5Backend } from "react-dnd-html5-backend";
+
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error('Root element #root not found. Ensure public/index.html has <div id="root"></div>.');
 }
 
-const root = ReactDOM.createRoot(rootEl as HTMLElement);
-root.render(
+createRoot(container).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* Uncomment providers here if your app expects them at the top level */}
+    {/* <UserProvider> */}
+    {/*   <DndProvider backend={HTML5Backend}> */}
+        <Router>
+          <React.Suspense fallback={<div />}>
+            <App />
+          </React.Suspense>
+        </Router>
+    {/*   </DndProvider> */}
+    {/* </UserProvider> */}
   </React.StrictMode>
 );
