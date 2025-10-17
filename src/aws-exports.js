46bec4c2e2d5362bdb4d50d8c1a1f97707a0c6d8
DahTruthStoryLab <jmbeausby@aol.com>
2025-10-17 18:35:52 -0400

    Update AWS region and Cognito configurations

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,27 +1,20 @@
 // src/aws-exports.js
 const awsmobile = {
-  aws_project_region: "us-east-2",
-  aws_cognito_region: "us-east-2",
-  aws_user_pools_id: "us-east-2_cia1t3",
-  aws_user_pools_web_client_id: "5scd368qedv001d8n687l3lbf2",
+  aws_project_region: "us-east-1",
+  aws_cognito_region: "us-east-1",
+  aws_user_pools_id: "us-east-1_M4ahggdKL",
+  aws_user_pools_web_client_id: "1e49rij6p6ejcknaglff9laber",
   oauth: {},
-
-  // Optional but recommended to keep Auth block aligned with your app client
   Auth: {
-    region: "us-east-2",
-    userPoolId: "us-east-2_cia1t3",
-    userPoolWebClientId: "5scd368qedv001d8n687l3lbf2",
+    region: "us-east-1",
+    userPoolId: "us-east-1_M4ahggdKL",
+    userPoolWebClientId: "1e49rij6p6ejcknaglff9laber",
     authenticationFlowType: "USER_PASSWORD_AUTH",
     mandatorySignIn: false,
   },
-
-  // Email-as-username setup
   aws_cognito_username_attributes: ["EMAIL"],
   aws_cognito_signup_attributes: ["EMAIL"],
-
-  // MFA off
   aws_cognito_mfa_configuration: "OFF",
   aws_cognito_verification_mechanisms: ["EMAIL"],
 };
-
 export default awsmobile;
