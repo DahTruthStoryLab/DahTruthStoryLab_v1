af891cf136d47e949bd3b3edd8703b5721014834
DahTruthStoryLab <jmbeausby@aol.com>
2025-10-18 14:05:33 -0400

    Update AWS Cognito Client ID in aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,15 +3,18 @@ const awsmobile = {
   aws_project_region: "us-east-1",
   aws_cognito_region: "us-east-1",
   aws_user_pools_id: "us-east-1_M4ahggdKL",
-  aws_user_pools_web_client_id: "1e49rij6p6ejcknaglff9laber",
+  aws_user_pools_web_client_id: "58ugnk7kp05dj9m7fbe1450k8", // ✅ Your App Client ID
+  
   oauth: {},
+  
   Auth: {
     region: "us-east-1",
     userPoolId: "us-east-1_M4ahggdKL",
-    userPoolWebClientId: "1e49rij6p6ejcknaglff9laber",
+    userPoolWebClientId: "58ugnk7kp05dj9m7fbe1450k8", // ✅ Your App Client ID
     authenticationFlowType: "USER_PASSWORD_AUTH",
     mandatorySignIn: false,
   },
+  
   aws_cognito_username_attributes: ["EMAIL"],
   aws_cognito_signup_attributes: ["EMAIL"],
   aws_cognito_mfa_configuration: "OFF",
