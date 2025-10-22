c85fdfe4795ee684a273982e1c11c7eb48a6152c
DahTruthStoryLab <jmbeausby@aol.com>
2025-10-22 18:06:07 -0400

    Delete src/lib/aiRoutes.js

--- src/lib/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import aiRoutes from "./aiRoutes.js";
-
-const app = express();
-app.use(express.json());
-app.use("/api/ai", aiRoutes);
-
-app.listen(process.env.PORT || 3001);
