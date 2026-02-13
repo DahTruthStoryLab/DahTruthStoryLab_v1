b3c20e0e99b2053ca083e1ddd84ae2087926088a
DahTruthStoryLab <jmbeausby@aol.com>
2026-02-13 18:33:03 -0500

    Add PoetryLab component to storylab page

---
+++ src/pages/storylab/PoetryLab.jsx
@@ -0,0 +1,8 @@
+src/pages/storylab/PoetryLab.jsx
+
+import React from "react";
+import PoetryModule from "../../components/storylab/PoetryModule";
+
+export default function PoetryLab() {
+  return <PoetryModule />;
+}
