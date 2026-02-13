0d6467a8af8d2d7ac631bff0d558bfcf9a6f1d6b
DahTruthStoryLab <jmbeausby@aol.com>
2026-02-13 18:33:51 -0500

    Create FictionLab component for FictionModule

---
+++ src/pages/storylab/FictionLab.jsx
@@ -0,0 +1,8 @@
+src/pages/storylab/FictionLab.jsx
+
+import React from "react";
+import FictionModule from "../../components/storylab/FictionModule";
+
+export default function FictionLab() {
+  return <FictionModule />;
+}
