ee24e2602cb4997b936be13b4e341d11b50b71d5
DahTruthStoryLab <jmbeausby@aol.com>
2026-02-13 18:31:55 -0500

    Add NonfictionLab component to render NonFictionModule

--- src/pages/storylab/NonFictionModule.jsx
+++ src/pages/storylab/NonFictionModule.jsx
@@ -1,3 +1,8 @@
 src/components/storylab/NonFictionModule.jsx
 
+import React from "react";
+import NonFictionModule from "../../components/storylab/NonFictionModule";
 
+export default function NonfictionLab() {
+  return <NonFictionModule />;
+}
