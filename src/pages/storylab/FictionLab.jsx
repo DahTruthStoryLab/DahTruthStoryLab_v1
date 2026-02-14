74da1a943d2cb08314c066f9340386abddb47890
DahTruthStoryLab <jmbeausby@aol.com>
2026-02-14 13:20:37 -0500

    Delete src/pages/storylab/FictionLab.jsx

--- src/pages/storylab/FictionLab.jsx
+++
@@ -1,8 +0,0 @@
-src/pages/storylab/FictionLab.jsx
-
-import React from "react";
-import FictionModule from "../../components/storylab/FictionModule";
-
-export default function FictionLab() {
-  return <FictionModule />;
-}
