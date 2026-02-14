bf6bee46c23072dc8dfbd1440319cde220f192a2
DahTruthStoryLab <jmbeausby@aol.com>
2026-02-14 13:21:03 -0500

    Delete src/pages/storylab/PoetryLab.jsx

--- src/pages/storylab/PoetryLab.jsx
+++
@@ -1,8 +0,0 @@
-src/pages/storylab/PoetryLab.jsx
-
-import React from "react";
-import PoetryModule from "../../components/storylab/PoetryModule";
-
-export default function PoetryLab() {
-  return <PoetryModule />;
-}
