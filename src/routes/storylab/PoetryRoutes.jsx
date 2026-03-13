6afc706148c6628e76424b1d85871fdd26e62525
DahTruthStoryLab <jmbeausby@aol.com>
2026-03-13 11:02:54 -0400

    Remove lazy loading for Poetry routes

--- src/routes/storylab/PoetryRoutes.jsx
+++ src/routes/storylab/PoetryRoutes.jsx
@@ -1,29 +1,26 @@
 // src/routes/storylab/PoetryRoutes.jsx
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import PoetryLab from "../../pages/storylab/PoetryLab";
 import PoetryLayout from "../../components/storylab/PoetryLayout";
-
-const PoetryLab = lazy(() => import("../../pages/storylab/PoetryLab"));
-const CraftLab = lazy(() => import("../../pages/storylab/poetry/CraftLab"));
-const RevisionLab = lazy(() => import("../../pages/storylab/poetry/RevisionLab"));
-const VoiceIdentity = lazy(() => import("../../pages/storylab/poetry/VoiceIdentity"));
-const SequenceBuilder = lazy(() => import("../../pages/storylab/poetry/SequenceBuilder"));
-const RemixLab = lazy(() => import("../../pages/storylab/poetry/RemixLab"));
+import CraftLab from "../../pages/storylab/poetry/CraftLab";
+import RevisionLab from "../../pages/storylab/poetry/RevisionLab";
+import VoiceIdentity from "../../pages/storylab/poetry/VoiceIdentity";
+import SequenceBuilder from "../../pages/storylab/poetry/SequenceBuilder";
+import RemixLab from "../../pages/storylab/poetry/RemixLab";
 
 export default function PoetryRoutes() {
   return (
-    <Suspense fallback={<div style={{padding:32}}>Loading...</div>}>
-      <Routes>
-        <Route index element={<PoetryLab />} />
-        <Route element={<PoetryLayout />}>
-          <Route path="craft" element={<CraftLab />} />
-          <Route path="revision" element={<RevisionLab />} />
-          <Route path="voice" element={<VoiceIdentity />} />
-          <Route path="sequence" element={<SequenceBuilder />} />
-          <Route path="remix" element={<RemixLab />} />
-        </Route>
-        <Route path="*" element={<Navigate to="." replace />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route index element={<PoetryLab />} />
+      <Route element={<PoetryLayout />}>
+        <Route path="craft" element={<CraftLab />} />
+        <Route path="revision" element={<RevisionLab />} />
+        <Route path="voice" element={<VoiceIdentity />} />
+        <Route path="sequence" element={<SequenceBuilder />} />
+        <Route path="remix" element={<RemixLab />} />
+      </Route>
+      <Route path="*" element={<Navigate to="." replace />} />
+    </Routes>
   );
 }
