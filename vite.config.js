c675eacd15b77a0b5230c4d8e3ac1be13bb9410b
DahTruthStoryLab <jmbeausby@aol.com>
2026-05-06 17:11:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
-    react(),
-   VitePWA({
-  registerType: "autoUpdate",
-  workbox: {
-    navigateFallback: null,
-    maximumFileSizeToCacheInBytes: 6 * 1024 * 1024
-  },
-      manifest: {
-        name: "DahTruth StoryLab",
-        short_name: "StoryLab",
-        start_url: "/",
-        scope: "/",
-        display: "standalone",
-        background_color: "#1e3a5f",
-        theme_color: "#1e3a5f",
-        icons: [
-          {
-            src: "/icons/icon-192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icons/icon-512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
+    react()
   ],
   build: {
     minify: false,
